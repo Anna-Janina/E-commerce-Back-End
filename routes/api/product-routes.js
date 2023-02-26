fc6a3a0654594d7f94dc655f0a99e17c976f7105
Anna <annajaninaschmidt@gmail.com>
2023-02-26 16:29:18 +1100

    models - category.js added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      }
+    ]
+  })
+})
+.then
+;
 
 // get one product
 router.get('/:id', (req, res) => {
